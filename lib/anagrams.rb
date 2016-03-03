bfab0316bc4bd2fcb848f071a78ee570a1b6689a
webbedfooty <patrickwiechert@yahoo.com>
2016-03-03 03:10:25 +0000

    Updated!

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,25 +1,33 @@
 def anagrams?(str1, str2)
+
 str1 = "chaser"
 str2 = "search"
 
 ana1 = Array.new
 ana2 = Array.new
 
-ana1 = [str1.count('a'), str1.count('b'), str1.count('c'),
-str1.count('d'), str1.count('e'), str1.count('f'), str1.count('g'),
-str1.count('h'), str1.count('i'), str1.count('j'), str1.count('k'),
-str1.count('l'), str1.count('m'), str1.count('n'), str1.count('o'),
-str1.count('p'), str1.count('q'), str1.count('r'), str1.count('s'),
-str1.count('t'), str1.count('u'), str1.count('v'), str1.count('w'),
-str1.count('x'), str1.count('y'), str1.count('z')]
 
-ana2 = [str2.count('a'), str2.count('b'), str2.count('c'),
-str2.count('d'), str2.count('e'), str2.count('f'), str2.count('g'),
-str2.count('h'), str2.count('i'), str2.count('j'), str2.count('k'),
-str2.count('l'), str2.count('m'), str2.count('n'), str2.count('o'),
-str2.count('p'), str2.count('q'), str2.count('r'), str2.count('s'),
-str2.count('t'), str2.count('u'), str2.count('v'), str2.count('w'),
-str2.count('x'), str2.count('y'), str2.count('z')]
+("a".."z").each do | letter |
+   ana1 << str1.count( letter)
+ end
+
+("a".."z").each do | letter |
+   ana2 << str2.count( letter)
+end
 
 ana1 == ana2
+
 end
+
+#Additional comments
+# 1 Remove the lines str1 = "chaser" and str2 = "search" and see if your program still passes all the tests
+
+# I did, I had the alphabet forwards for ana1 and backwards for ana2 and then also took out random letters as well.
+# Extensive testing. Well sort of ;)
+
+
+# 2	Look around and see if there's a better way to do the part where you build that array. 
+# Note that something like [ str1.count('a'), str1.count('b')....] (the ellipsis is used in the grammatical, written English way here) is a list, 
+# but the order doesn't really matter. Could you instead use a hash? Is there a better way still? I'm giving you permission here to google it.
+
+# Thanks again for the help
